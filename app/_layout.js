fdb1ce65e510e66a5a2f9a90dd02a5fe576a3f48
Jxkeorton <jakeorton99@gmail.com>
2025-07-26 11:33:00 +0100

    Add reset password user flow

--- app/_layout.js
+++ app/_layout.js
@@ -6,6 +6,25 @@ import { QueryProvider } from "../providers/QueryProvider";
 import { AuthProvider } from "../providers/AuthProvider";
 import Toast from 'react-native-toast-message';
 
+// Handles the user visiting the app to reset their password after clicking the link within their emails
+const handlePasswordResetRedirect = (url) => {
+    console.log('Deep link received:', url);
+    
+    const parsed = Linking.parse(url);
+    
+    // Check if it's a password reset link
+    if (parsed.path === '/auth//ResetPasswordConfirm') {
+        const { access_token, refresh_token } = parsed.queryParams || {};
+        
+        if (access_token && refresh_token) {
+            router.push({
+                pathname: '/(auth)//ResetPasswordConfirm',
+                params: { access_token, refresh_token }
+            });
+        }
+    }
+};
+
 export default function Layout(){
     return (
         <SafeAreaProvider>
