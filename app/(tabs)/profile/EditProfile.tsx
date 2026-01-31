e628206276a1eab961d17c271ca95492b42fe51c
Jake <jakeorton99@gmail.com>
2026-01-31 15:01:13 +0000

    ADD: image picker hook

--- app/(tabs)/profile/EditProfile.tsx
+++ app/(tabs)/profile/EditProfile.tsx
@@ -2,12 +2,10 @@ import { FontAwesome } from "@expo/vector-icons";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useQuery } from "@tanstack/react-query";
 import { Image } from "expo-image";
-import * as ImagePicker from "expo-image-picker";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
-  Alert,
   KeyboardAvoidingView,
   Platform,
   ScrollView,
@@ -23,6 +21,7 @@ import {
   ControlledPaperEmailInput,
   ControlledPaperTextInput,
 } from "../../../components/form";
+import { useImagePicker } from "../../../hooks/useImagePicker";
 import {
   UpdateProfileData,
   useUpdateProfile,
@@ -41,15 +40,7 @@ const EditProfile = () => {
   const [image, setImage] = useState<string | null>(null);
   const client = useKyClient();
   const { mutateAsync: uploadImageMutation, error: uploadError } =
-    useUploadImage({
-      onError: (error) => {
-        Toast.show({
-          type: "error",
-          text1: "Image Upload Failed",
-          text2: error.message,
-        });
-      },
-    });
+    useUploadImage();
 
   const {
     control,
@@ -166,31 +157,15 @@ const EditProfile = () => {
   });
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library.
-    // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
-    // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
-    // so the app users aren't surprised by a system dialog after picking a video.
-    // See "Invoke permissions for videos" sub section for more details.
-    const permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const result = await useImagePicker({});
 
-    if (!permissionResult.granted) {
-      Alert.alert(
-        "Permission required",
-        "Permission to access the media library is required.",
-      );
-      return;
-    }
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images", "videos"],
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets?.[0]?.uri || "");
+    if (result) {
+      setImage(result[0]?.uri || "");
+    } else {
+      Toast.show({
+        type: "info",
+        text1: "No image selected",
+      });
     }
   };
 
---
+++ hooks/useImagePicker.ts
@@ -0,0 +1,38 @@
+import * as ImagePicker from "expo-image-picker";
+import { Alert } from "react-native";
+
+interface UseImagePickerProps {
+  imagePickerOptions?: ImagePicker.ImagePickerOptions;
+}
+
+export const useImagePicker = async ({
+  imagePickerOptions,
+}: UseImagePickerProps) => {
+  // No permissions request is necessary for launching the image library.
+  // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
+  // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
+  // so the app users aren't surprised by a system dialog after picking a video.
+  // See "Invoke permissions for videos" sub section for more details.
+  const permissionResult =
+    await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+  if (!permissionResult.granted) {
+    Alert.alert(
+      "Permission required",
+      "Permission to access the media library is required.",
+    );
+    return;
+  }
+
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ["images"],
+    quality: 1,
+    ...imagePickerOptions,
+  });
+
+  if (!result.canceled) {
+    return result.assets;
+  } else {
+    return undefined;
+  }
+};
